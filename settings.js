a85e3a32bbb07d5f43435b3c7e7e5977819b2ea3
Erennnnnn0 <brahim1abc8888@gmail.com>
2024-06-05 07:22:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,8 +22,8 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
+global.ownernomer = "212690349201" //ur number
+global.premium = ['212690349201'] //ur premium number
 global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
