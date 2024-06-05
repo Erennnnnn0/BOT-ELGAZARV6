25542e86eb068933a77595a831f255fe36102912
Erennnnnn0 <brahim1abc8888@gmail.com>
2024-06-05 07:22:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
+global.owner = ['212690349201'] //ur owner number
 global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
 global.ytname = "YT: ELGAZAR" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
@@ -30,7 +30,7 @@ global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur websi
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
 global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
+global.author = "⍣212690349201⍣" //ur sticker watermark author
 global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
 
 // Other
